c149a3828a4a030378c6b2e9d3a8d1c312491161
ShineWaiYanAung <chantharpro89@gmail.com>
2025-07-03 11:30:21 +0700

    FixingAgain2

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1895154608"
+    serviceWorkerVersion: "2541910674"
   }
 });
