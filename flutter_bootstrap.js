4972a216aea57515effc55d3376e0a769b17ec9d
ShineWaiYanAung <chantharpro89@gmail.com>
2025-07-10 17:12:19 +0700

    LaunchingPortfolio1

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3868715951"
+    serviceWorkerVersion: "2110257519"
   }
 });
